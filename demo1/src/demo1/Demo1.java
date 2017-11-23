b7ac8c2ce8d659a7aca0f1aede16a7bb37058d18
constantcurrent <17854217017@163.com>
2017-11-23 16:00:35 +0800

    kelong hou

--- demo1/src/demo1/Demo1.java
+++ demo1/src/demo1/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		String str=new String();
-		str="java start";//这一步可以修改
+		str="java start";
 		System.out.println(str);
 		System.out.println(str+" with no end");
 
