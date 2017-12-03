7fd17f77dd7796f7933b2c628a0d1521b9c97aae
hsy_git <17854217017@163.com>
2017-12-02 19:55:02 -0800

    you win when you sing

--- demo1/src/demo1/Demo1.java
+++ demo1/src/demo1/Demo1.java
@@ -5,7 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		String str=new String();
 		str="java start";//
-		System.out.println(str);
+boolean str2=str.matches("j?(ava)+");
+		System.out.println(str2);
 		System.out.println(str+" with dddsdscdcsscscscscso end");
 
 	}
